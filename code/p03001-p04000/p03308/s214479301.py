6d86e97ae7c53f4abe9cb35861955e9b465eb8fc
xcj <2447518907@qq.com>
2026-01-03 07:23:27 +0800

    添加代码文件批次 1820

---
+++ code/p03001-p04000/p03308/s214479301.py
@@ -0,0 +1,30 @@
+from statistics import mean, median,variance,stdev
+import sys
+import math
+import fractions
+
+def j(q):
+    if q==1: print("Yes")
+    else:print("No")
+    exit(0)
+
+
+def ct(x,y):
+    if (x>y):print("")
+    elif (x<y): print("")
+    else: print("")
+
+def ip():
+    return int(input())
+
+x = ip()                              #入力整数1つ
+#x,y= (int(i) for i in input().split())      #入力整数横2つ
+#x,y,z = (int(i) for i in input().split())    #入力整数横3つ
+#n,m,x,y= (int(i) for i in input().split())  #入力整数横4つ
+a = [int(i) for i in input().split()]        #入力整数配列
+#s = input()                                  #入力文字列
+#a = input().split()                          #入力文字配列
+
+#jの変数はしようできないので注意
+
+print(max(a)-min(a))
---
+++ code/p03001-p04000/p03308/s666706936.py
@@ -0,0 +1,21 @@
+import sys, re
+from collections import deque, defaultdict, Counter
+from math import ceil, sqrt, hypot, factorial, pi, sin, cos, radians
+from heapq import heappop, heappush, heapify, heappushpop
+from itertools import permutations, combinations, product
+from operator import itemgetter, mul
+from copy import deepcopy
+from string import ascii_lowercase, ascii_uppercase, digits
+
+def input(): return sys.stdin.readline().strip()
+def INT(): return int(input())
+def MAP(): return map(int, input().split())
+def LIST(): return list(map(int, input().split()))
+sys.setrecursionlimit(10 ** 9)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+N = INT()
+A = LIST()
+
+print(max(A)-min(A))
