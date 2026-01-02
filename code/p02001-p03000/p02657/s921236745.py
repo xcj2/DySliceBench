57137fbc44de00c8653c7c281dbdf0432dda1dfe
xcj <2447518907@qq.com>
2026-01-03 04:44:05 +0800

    添加代码文件批次 555

---
+++ code/p02001-p03000/p02657/s921236745.py
@@ -0,0 +1,18 @@
+import sys
+# sys.setrecursionlimit(pow(10, 6))
+# sys.stdin = open("input.txt", "r")
+# sys.stdout = open("output.txt", "w")
+mod = pow(10, 9) + 7
+mod2 = 998244353
+def data(): return sys.stdin.readline().strip()
+def out(*var, end="\n"): sys.stdout.write(' '.join(map(str, var))+end)
+def l(): return list(sp())
+def sl(): return list(ssp())
+def sp(): return map(int, data().split())
+def ssp(): return map(str, data().split())
+def l1d(n, val=0): return [val for i in range(n)]
+def l2d(n, m, val=0): return [l1d(n, val) for j in range(m)]
+
+
+a, b = sp()
+out(a * b)
