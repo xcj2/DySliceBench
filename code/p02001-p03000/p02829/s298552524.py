5c56fdad278e54eea1b6199e2afc1b17c2575cb7
xcj <2447518907@qq.com>
2026-01-03 05:40:22 +0800

    添加代码文件批次 1009

---
+++ code/p02001-p03000/p02829/s298552524.py
@@ -0,0 +1,40 @@
+import copy
+import heapq
+import math
+
+def j(n):
+    if n:print("Yes")
+    else:print("No")
+    exit(0)
+rem = 10 ** 9 + 7
+
+"""
+def ct(x,y):
+    if (x>y):print("+")
+    elif (x<y): print("-")
+    else: print("?")
+"""
+
+def ip():
+    return int(input())
+def iprow():
+    return [int(i) for i in input().split()]
+def ips():
+    return (int(i) for i in input().split())
+def ipmultiplerow(n):
+    a = []
+    for i in range(n):
+        a.append(ip())
+    return a
+def printrow(a):
+    for i in a:
+        print(i)
+
+a = ip()
+b = ip()
+if a*b == 2:
+    print(3)
+if a*b == 6:
+    print(1)
+if a*b == 3:
+    print(2)
