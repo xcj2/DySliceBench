771323357fa9e1aa238f4f534fba1a0fc4888dd6
xcj <2447518907@qq.com>
2026-01-03 06:08:41 +0800

    添加代码文件批次 1235

---
+++ code/p02001-p03000/p02945/s763658175.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+from collections import defaultdict,deque
+from heapq import heappush, heappop
+from bisect import *
+import sys, random, itertools, math
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+sqrt = math.sqrt
+def LI(): return list(map(int, input().split()))
+def LF(): return list(map(float, input().split()))
+def LI_(): return list(map(lambda x: int(x)-1, input().split()))
+def II(): return int(input())
+def IF(): return float(input())
+def LS(): return list(map(list, input().split()))
+def S(): return list(input().rstrip())
+def IR(n): return [II() for _ in range(n)]
+def LIR(n): return [LI() for _ in range(n)]
+def FR(n): return [IF() for _ in range(n)]
+def LFR(n): return [LI() for _ in range(n)]
+def LIR_(n): return [LI_() for _ in range(n)]
+def SR(n): return [S() for _ in range(n)]
+def LSR(n): return [LS() for _ in range(n)]
+mod = 1000000007
+inf = float('INF')
+
+#A
+def A():
+    a, b = LI()
+    print(max(a + b, a - b, a * b))
+    return
+
+#B
+def B():
+    k, x = LI()
+    ans = []
+    for i in range(x - k + 1, x + k):
+        ans.append(i)
+    print(*ans)
+    return
+
+#C
+def C():
+    n = II()
+    d = defaultdict(int)
+    for _ in range(n):
+        s = S()
+        s.sort()
+        d[tuple(s)] += 1
+    ans = 0
+    for i in d.values():
+        ans += i * (i - 1) // 2
+    print(ans)
+    return
+
+#D
+def D():
+    n, m = LI()
+    ab = LIR(n)
+    ab.sort(key=lambda x: (x[1], x[0]), reverse=True)
+    ans = 0
+    for i in range(n):
+        if m >= ab[i][0]:
+            ans += ab[i][1]
+            m -= 1
+    print(ans)
+    return
+
+#E
+def E():
+    return
+
+#F
+def F():
+    return
+
+#Solve
+if __name__ == '__main__':
+    A()
---
+++ code/p02001-p03000/p02945/s835750806.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(10**7)
+def LI(): return [int(x) for x in sys.stdin.readline().split()]
+def LI_(): return [int(x) - 1 for x in sys.stdin.readline().split()]
+def LF(): return [float(x) for x in sys.stdin.readline().split()]
+def LS(): return sys.stdin.readline().split()
+def II(): return int(sys.stdin.readline())
+def SI(): return sys.stdin.readline().strip()
+INF = 10 ** 18
+MOD = 10 ** 9 + 7
+
+debug = True
+#debug = False
+
+def dprint(*objects):
+    if debug == True:
+        print(*objects)
+
+def solve():
+    a, b = LI()
+
+    a1 = a + b
+    a2 = a - b
+    a3 = a * b
+    print(max(a3, max(a1, a2)))
+
+solve()
