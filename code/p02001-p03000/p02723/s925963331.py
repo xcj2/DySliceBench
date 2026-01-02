6967e9aaad638377292d30d2d3f7273dde551506
xcj <2447518907@qq.com>
2026-01-03 05:00:25 +0800

    添加代码文件批次 688

---
+++ code/p02001-p03000/p02723/s925963331.py
@@ -0,0 +1,23 @@
+import math,itertools,fractions,heapq,collections,bisect,sys,queue,copy
+
+sys.setrecursionlimit(10**7)
+inf=10**20
+mod=10**9+7
+dd=[(-1,0),(0,1),(1,0),(0,-1)]
+ddn=[(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1)]
+
+def LI(): return [int(x) for x in sys.stdin.readline().split()]
+def LF(): return [float(x) for x in sys.stdin.readline().split()]
+def I(): return int(sys.stdin.readline())
+def LS(): return sys.stdin.readline().split()
+def S(): return input()
+
+def main():
+  s=S()
+
+  if s[2]==s[3] and s[4]==s[5]:
+    return 'Yes'
+  return 'No'
+
+# main()
+print(main())
