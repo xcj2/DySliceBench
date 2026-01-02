4d579815838a11a9d91a7b877f595666f196d6bd
xcj <2447518907@qq.com>
2026-01-03 05:26:10 +0800

    添加代码文件批次 895

---
+++ code/p02001-p03000/p02784/s214578329.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+
+# 入力を整数に変換して受け取る
+def input_int():
+    return int(input())
+
+
+# マイナス1した値を返却
+def int1(x):
+    return int(x) - 1
+
+
+# 半角スペース区切り入力をIntに変換してMapで受け取る
+def input_to_int_map():
+    return map(int, input().split())
+
+
+# 半角スペース区切り入力をIntに変換して受け取る
+def input_to_int_tuple():
+    return tuple(map(int, input().split()))
+
+
+# 半角スペース区切り入力をIntに変換してマイナス1した値を受け取る
+def input_to_int_tuple_minus1():
+    return tuple(map(int1, input().split()))
+
+
+def main():
+    h, n = input_to_int_map()
+    a = input_to_int_tuple()
+
+    ret = "Yes"
+    if h - sum(a) > 0:
+        ret = "No"
+
+    return ret
+
+
+if __name__ == "__main__":
+    print(main())
