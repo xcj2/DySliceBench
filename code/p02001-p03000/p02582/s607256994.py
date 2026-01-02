738419473198e923fe63891389e2d9de68dc99ad
xcj <2447518907@qq.com>
2026-01-03 04:27:53 +0800

    添加代码文件批次 424

---
+++ code/p02001-p03000/p02582/s607256994.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**7)
+from collections import Counter, deque
+from collections import defaultdict
+from itertools import combinations, permutations, accumulate, groupby, product
+from bisect import bisect_left,bisect_right
+from heapq import heapify, heappop, heappush
+from math import floor, ceil,pi,factorial
+from operator import itemgetter
+def I(): return int(input())
+def MI(): return map(int, input().split())
+def LI(): return list(map(int, input().split()))
+def LI2(): return [int(input()) for i in range(n)]
+def MXI(): return [[LI()]for i in range(n)]
+def SI(): return input().rstrip()
+def printns(x): print('\n'.join(x))
+def printni(x): print('\n'.join(list(map(str,x))))
+inf = 10**17
+mod = 10**9 + 7
+
+s=SI()
+if s[0]==s[1]==s[2]=="S":
+    print(0)
+    sys.exit()
+count=0
+if s[0]==s[1]=="R":
+    count+=1
+if s[1]==s[2]=="R":
+    count+=1
+print(count+1)
+    
+            
+            
