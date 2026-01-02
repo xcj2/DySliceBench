505836ce1fb5c79fab18220b0030db9e4797f5e5
xcj <2447518907@qq.com>
2026-01-03 04:55:47 +0800

    添加代码文件批次 650

---
+++ code/p02001-p03000/p02711/s014286297.py
@@ -0,0 +1,17 @@
+import sys
+import math
+def i():return int(sys.stdin.readline().replace("\n",""))
+def i2():return map(int,sys.stdin.readline().replace("\n","").split())
+def s():return str(sys.stdin.readline().replace("\n",""))
+def l():return list(sys.stdin.readline().replace("\n",""))
+def intl():return [int(k) for k in sys.stdin.readline().replace("\n","").split()]
+def lx():return list(map(lambda x:int(x)*-1,sys.stdin.readline().replace("\n","").split()))
+def t():return tuple(map(int,sys.stdin.readline().replace("\n","").split()))
+
+if __name__ == "__main__":pass
+m = s()
+for i in range(3):
+    if m[i] == "7":
+        print("Yes")
+        exit()
+print("No")
---
+++ code/p02001-p03000/p02711/s770466336.py
@@ -0,0 +1,43 @@
+import sys
+from io import StringIO
+import unittest
+
+def resolve():
+    n = input()
+
+    if n[0] == "7":
+        print("Yes")
+        return
+    if n[1] == "7":
+        print("Yes")
+        return
+    if n[2] == "7":
+        print("Yes")
+        return
+    print("No")
+    return
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+    def test_入力例_1(self):
+        input = """117"""
+        output = """Yes"""
+        self.assertIO(input, output)
+    def test_入力例_2(self):
+        input = """123"""
+        output = """No"""
+        self.assertIO(input, output)
+    def test_入力例_3(self):
+        input = """777"""
+        output = """Yes"""
+        self.assertIO(input, output)
+
+if __name__ == "__main__":
+    #unittest.main()
+    resolve()
