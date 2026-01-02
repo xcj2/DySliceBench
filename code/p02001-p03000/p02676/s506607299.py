3c0f60e58c2453eb1ab2a6e105be136676e96842
xcj <2447518907@qq.com>
2026-01-03 04:47:04 +0800

    添加代码文件批次 579

---
+++ code/p02001-p03000/p02676/s506607299.py
@@ -0,0 +1,76 @@
+import time
+startTimeProblem=time.time()
+
+import fileinput, sys, itertools, functools
+from math import *
+from bisect import *
+from heapq import *
+from collections import *
+
+def lcm(a, b):  
+    return (a*b)/gcd(a, b)
+
+class InputHelper:
+    def __init__(self):
+        self.myinput = fileinput.input()
+
+    def isLocal(self):
+        return not fileinput.isstdin()
+
+    def int(self):
+        return int(self.myinput.readline().rstrip())
+
+    def ints(self):
+        return [int(_) for _ in self.myinput.readline().rstrip().split()]
+
+    def str(self):
+        return self.myinput.readline().rstrip()
+
+    def strs(self):
+        return [_ for _ in self.myinput.readline().rstrip().split()]
+
+class OutputHelper:
+    def int(self, a):
+        print(a)    
+
+    def ints(self, a):  
+        print(" ".join([str(_) for _ in a]))
+    
+    def intsNL(self, a):
+        for _ in a:
+            print(_)
+    
+    def str(self, s):
+        print(s)
+
+    def strs(self, s):
+        print(" ".join([_ for _ in s]))
+
+    def strsNL(self, s):
+        for st in s:
+            print(st)
+
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+In = InputHelper()
+Out = OutputHelper()
+
+######################################
+
+K = In.int()
+S = In.str()
+
+if len(S)<=K:
+    Out.str(S)
+else:
+    Out.str(S[0:K] + "...")
+
+######################################
+
+if len(sys.argv)>2 and sys.argv[2]=="TIMEIT":
+    fin = (time.time()-startTimeProblem)*1000
+    print("{:.2f}".format(fin) + "ms")
---
+++ code/p02001-p03000/p02676/s625872734.py
@@ -0,0 +1,39 @@
+import sys, bisect, math, itertools, heapq, collections
+from operator import itemgetter
+# a.sort(key=itemgetter(i)) # i番目要素でsort
+from functools import lru_cache
+# @lru_cache(maxsize=None)
+sys.setrecursionlimit(10**8)
+input = sys.stdin.readline
+INF = float('inf')
+mod = 10**9 + 7
+eps = 10**-7
+
+
+def inp():
+    '''
+    一つの整数
+    '''
+    return int(input())
+
+
+def inpl():
+    '''
+    一行に複数の整数
+    '''
+    return list(map(int, input().split()))
+
+
+def str_inp():
+    '''
+    文字列をリストとして読み込む
+    '''
+    return list(input()[:-1])
+
+
+k = inp()
+s = str_inp()
+if len(s) <= k:
+    print("".join(s))
+else:
+    print("".join(s[:k]) + "...")
