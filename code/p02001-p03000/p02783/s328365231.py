c845902bb8ec97c46bafdd0e2e572aa2d242e19f
xcj <2447518907@qq.com>
2026-01-03 05:26:03 +0800

    添加代码文件批次 894

---
+++ code/p02001-p03000/p02783/s328365231.py
@@ -0,0 +1,21 @@
+import sys
+import math
+import heapq
+sys.setrecursionlimit(10**7)
+INTMAX = 9223372036854775807
+INTMIN = -9223372036854775808
+DVSR = 1000000007
+def POW(x, y): return pow(x, y, DVSR)
+def INV(x, m=DVSR): return pow(x, m - 2, m)
+def DIV(x, y, m=DVSR): return (x * INV(y, m)) % m
+def LI(): return [int(x) for x in sys.stdin.readline().split()]
+def LF(): return [float(x) for x in sys.stdin.readline().split()]
+def LS(): return sys.stdin.readline().split()
+def II(): return int(sys.stdin.readline())
+def FLIST(n):
+    res = [1]
+    for i in range(1, n+1): res.append(res[i-1]*i%DVSR)
+    return res
+
+H, A = LI()
+print(H // A + (1 if H%A else 0))
---
+++ code/p02001-p03000/p02783/s470225035.py
@@ -0,0 +1,25 @@
+import math,string,itertools,fractions,heapq,collections,re,array,bisect,sys,random,time,queue,copy
+
+sys.setrecursionlimit(10**7)
+inf=10**20
+mod=10**9+7
+dd=[(-1,0),(0,1),(1,0),(0,-1)]
+ddn=[(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1)]
+
+def LI(): return [int(x) for x in sys.stdin.readline().split()]
+def LI_(): return [int(x)-1 for x in sys.stdin.readline().split()]
+def I(): return int(sys.stdin.readline())
+def LS(): return sys.stdin.readline().split()
+def S(): return input()
+
+def main():
+  h,a=LI()
+  c=0
+  for _ in range(100000):
+    if h<=0:
+      return c
+    h-=a
+    c+=1
+
+# main()
+print(main())
