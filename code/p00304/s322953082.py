92c656e9d09de15620876d1b08d4f2a7b422eac6
xcj <2447518907@qq.com>
2026-01-03 03:21:05 +0800

    添加代码文件批次 360

---
+++ code/p00304/s322953082.py
@@ -0,0 +1,93 @@
+'''aizu 0309
+http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0309
+'''
+# encoding: 'utf-8'
+
+N = 1000000007
+
+def main():
+    node_n = int(input())
+    nodes = list()
+    for _ in range(node_n):
+        nodes.append(Node(input()))
+    for _ in range(node_n - 1):
+        i, j = tuple([int(x) for x in input().split()])
+        nodes[i-1].children.append(nodes[j-1])
+        nodes[i-1].index.append(j-1)
+
+    print(reduce_to_top(nodes).n % N)
+
+
+def product(iterator):
+    result = 1
+    for element in iterator:
+        result *= element % N
+        result %= N
+    return result
+
+
+def step_reducing(nodes):
+    last_parents = [node for node in nodes if node is not None and node.is_last_parent()]
+    for parent in last_parents:
+        parent.n = parent.number_of_cases()
+        parent.children = list()
+        for i in parent.index:
+            nodes[i] = None
+    return nodes
+
+
+def reduce_to_top(nodes):
+    while nodes[1:] != [None]*(len(nodes)-1):
+        nodes = step_reducing(nodes)
+    return nodes[0]
+
+
+class Node:
+
+    def __init__(self, kind, children=None, n=None, index=None):
+        self.kind = kind
+        if children is None:
+            children = list()
+        if index is None:
+            index = list()
+        self.children = children
+        self.index = index
+        self.n = n
+
+    def number_of_cases(self):
+        if self.n is not None:
+            return self.n
+
+        cases = self.generate_cases()
+        if "E" in self.kind:
+            result = product(cases)
+            if "?" in self.kind:
+                result += 1
+            return result % N
+
+        elif "A" in self.kind:
+            result = sum(cases)
+            if "?" in self.kind:
+                result += 1
+            return result % N
+
+        elif "R" in self.kind:
+            result = product((x+1 for x in cases))
+            if "?" not in self.kind:
+                result -= 1
+            return result % N
+
+        else:
+            raise Exception("unreachable.")
+
+    def generate_cases(self):
+        for child in self.children:
+            yield child.number_of_cases()
+
+    def is_last_parent(self):
+        bools = [child.children == [] for child in self.children]
+        return bools and all(bools)
+
+
+if __name__ == '__main__':
+    main()
---
+++ code/p00315/s142975547.py
@@ -0,0 +1,40 @@
+def next(N, i):
+    return ((N-i-1)+N)%N
+
+def getState(N, G, i, j):
+    return  G[i][j] == G[i][next(N, j)] and  G[i][j] == G[next(N, i)][j] and G[i][j] == G[next(N, i)][next(N, j)]
+
+def getInit(N, G):
+    dcnt = 0
+    for i in range(N//2):
+        for j in range(N//2):
+            if not getState(N, G, i, j): dcnt += 1
+    return dcnt
+
+C, N = map(int, input().split())
+#G = [[0]*N]*N
+G = [['N' for _ in range(N)] for _ in range(N)]
+for i in range(N):
+    str = input()
+    for j in range(N):
+        G[i][j] = int(str[j])
+
+ans = 0
+dcnt = getInit(N, G)
+if dcnt == 0: ans += 1
+
+for i in range(C-1):
+    k = int(input())
+    for j in range(k):
+        r, c = map(int, input().split())
+        r -= 1
+        c -= 1
+        pre = getState(N, G, r, c)
+        G[r][c] = 0 if G[r][c]== 1 else 1
+        post = getState(N, G, r, c)
+        if not pre and post: dcnt -= 1
+        elif  pre and not post: dcnt += 1
+    if dcnt == 0: ans += 1
+
+print(ans)
+
