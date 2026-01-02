3d7c2b52654139726ccf2bfa82d7bb96179a09e4
xcj <2447518907@qq.com>
2026-01-03 07:16:01 +0800

    添加代码文件批次 1763

---
+++ code/p03001-p04000/p03260/s026752627.py
@@ -0,0 +1,83 @@
+#!usr/bin/env python3
+from collections import defaultdict
+from collections import deque
+from heapq import heappush, heappop
+import sys
+import math
+import bisect
+import random
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def I(): return int(sys.stdin.readline())
+def LS():return list(map(list, sys.stdin.readline().split()))
+def S(): return list(sys.stdin.readline())[:-1]
+def IR(n):
+    l = [None for i in range(n)]
+    for i in range(n):l[i] = I()
+    return l
+def LIR(n):
+    l = [None for i in range(n)]
+    for i in range(n):l[i] = LI()
+    return l
+def SR(n):
+    l = [None for i in range(n)]
+    for i in range(n):l[i] = S()
+    return l
+def LSR(n):
+    l = [None for i in range(n)]
+    for i in range(n):l[i] = LS()
+    return l
+sys.setrecursionlimit(1000000)
+mod = 1000000007
+
+#A
+def A():
+    a,b,c = LI()
+    print(9*max(a,b,c)+a+b+c)
+    return
+
+#B
+def B():
+    a,b = LI()
+    if a*b %2:
+        print("Yes")
+    else:
+        print("No")
+    return
+
+#C
+def C():
+    k = I()
+    print(k//2-1)*(k//2)
+    return
+
+#D
+def D():
+    return
+
+#E
+def E():
+    return
+
+#F
+def F():
+    return
+
+#G
+def G():
+    return
+
+#H
+def H():
+    return
+
+#I
+def I_():
+    return
+
+#J
+def J():
+    return
+
+#Solve
+if __name__ == "__main__":
+    B()
